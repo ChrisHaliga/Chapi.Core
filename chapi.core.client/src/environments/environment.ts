b0aaa0394651446ed83b474d3aab7b3ea898ba2f
Chris Haliga <Christianhaliga@gmail.com>
2025-02-16 13:40:57 -0600

    client enviornment files

---
+++ chapi.core.client/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  apiRoot: "/api/",
+  auth: {
+    domain: "dev-czez0gt6kknfxpso.us.auth0.com",
+    clientId: "4vKfkq3y8dXLUDwcwJV5OdBrwHxbnAUA",
+    audience: "SandboxApp",
+    redirectUri: window.location.origin
+  }
+}
