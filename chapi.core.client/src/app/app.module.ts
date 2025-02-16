cf2877420d4ffeb39d2bbb9fdfbb8d6cdcb86522
Chris Haliga <Christianhaliga@gmail.com>
2025-02-16 13:16:04 -0600

    Added PWA Angular frontend

---
+++ chapi.core.client/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})], providers: [provideHttpClient(withInterceptorsFromDi())] })
+export class AppModule { }
