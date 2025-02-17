d9129941b69ffea06c87151a5731a4ff3359ff41
Chris Haliga <Christianhaliga@gmail.com>
2025-02-16 23:41:23 -0600

    homepage start

---
+++ chapi.core.client/src/pages/home/home.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'home',
+  standalone: false,
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.css']
+})
+
+export class HomePage {
+
+}
